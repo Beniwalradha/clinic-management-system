6fe3670aa182e51337e71cb5fb7740f823a9568d
Beniwalradha <beniwalradha9@gmail.com>
2025-07-10 13:26:08 +0530

    initial commit

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include<sstream>
 #include<algorithm>
 using namespace std;
+//class to store and manage patient details
 class patient {
 public:   
 //members 
@@ -24,7 +25,7 @@ public:
     
 };
 void patient::input(){
-//name input
+    //name input
     do{
         cout<<"Enter patient name :";
         getline(cin>>ws,name);
@@ -32,8 +33,8 @@ void patient::input(){
             cout<<"\nInvalid name!\n";
 
         }
-        
-    }while(!isValidName(name));
+    }
+    while(!isValidName(name));
     //age input
     do{
         cout<<"Enter patient age(0-120) :";
@@ -43,21 +44,24 @@ void patient::input(){
             cin.clear();
             cin.ignore(1000,'\n');
         }
-    }while(age<0||age>120||cin.fail());
+    }
+    while(age<0||age>120||cin.fail());
      //phone number input
     do{
         cout<<"Enter 10-digit phone number: ";
         getline(cin>>ws,phoneNumber);
-    }while(!isValidPhoneNumber(phoneNumber));
-    //illness inout
+    }
+    while(!isValidPhoneNumber(phoneNumber));
+    //illness input
     do{
         cout<<"Enter patient illness :";
         getline(cin>>ws,illness);
         if(!isValidName(illness)){
             cout<<"\nInvalid illness!\n";
         }
-    }while(!isValidName(illness));
-    //doctor name
+    }
+    while(!isValidName(illness));
+    //doctor name input
     do{
         cout<<"Enter doctor name :";
         getline(cin>>ws,doctor);
@@ -65,7 +69,8 @@ void patient::input(){
             cout<<"\nInvalid doctor name!"<<endl;
 
         }
-    }while(!isValidName(doctor));
+    }
+    while(!isValidName(doctor));
     //input visitDate
     do{
         cout<<"Enter visit date :";
@@ -74,9 +79,10 @@ void patient::input(){
             cout<<"\nInvalid visitdate!\n";
 
         }
-    }while(!isValidDate(visitDate));
+    }
+    while(!isValidDate(visitDate));
 }
-       
+//function to check validation name input      
 bool patient::isValidName(const string &str){
     if(str.empty()){
         return false;
@@ -88,20 +94,31 @@ bool patient::isValidName(const string &str){
     }
     return true;
 }
+//function to check validation date input      
 bool patient ::isValidDate(const string &date){
-    if(date.length()!=10||date[4]!='-'||date[7]!='-'){
+    int year,month,day;
+    char dash1,dash2;
+    stringstream ss(date);
+    ss>>year>>dash1>>month>>dash2>>day;
+    if(dash1!='-'|| dash2!='-'||month<1||month>12||day<1||day>31){
         return false;
-    }for(int i=0;i<date.size();++i){
-        if(i==4||i==7){
-            continue;
-        }
-        if((!isdigit(date[i]))){
-            return false;
-        }
     }
     return true;
+
+    // //if(date.length()!=10||date[4]!='-'||date[7]!='-'){
+    //     return false;
+    // }for(int i=0;i<date.size();++i){
+    //     if(i==4||i==7){
+    //         continue;
+    //     }
+    //     if((!isdigit(date[i]))){
+    //         return false;
+    //     }
+    // }
+    // return true;//
     
 }
+//function to check validation phone number input      
 bool patient:: isValidPhoneNumber(const string &number){
     if(number.length()!=10){
         cout<<"\nphone number must be 10 - digit\n";
@@ -119,13 +136,14 @@ bool patient:: isValidPhoneNumber(const string &number){
     }
     return true;
 }
+//display all patient details
 void patient::display() const{
     cout<<"\n--patient details--\n";
     cout<<" Name:  "<<name<<endl;
     cout<<" Age: "<<age<<endl;
     cout<<" phone number: "<<phoneNumber<<endl;
-    cout<<" Illness: "<<doctor<<endl;
-    cout<<" Doctor: "<<illness<<endl;
+    cout<<" Illness: "<<illness<<endl;
+    cout<<" Doctor: "<<doctor<<endl;
     cout<<"Visit Date: "<<visitDate<<endl;
 
 }
@@ -134,7 +152,8 @@ void savePatient(const patient &p){
     ofstream file("clinicManage.txt",ios::app);
     if(file.is_open()){
         file<<p.name<<","<<p.age<<","<<p.phoneNumber<<","<<p.doctor<<","<<p.illness<<","<<p.visitDate<<"\n";
-    }else{
+    }
+    else{
         cout<<"Error ! could not open file.\n";
     }
 }
@@ -166,9 +185,7 @@ readPatientsFromFile() {
     }
     return patients;
 }
-//writeToFile(after update delete)
-
-
+//overwrite file with updated data
 void saveAllPatientsToFile(const vector<patient>& patients) {
     ofstream file("clinicManage.txt");
     for (const auto& p : patients) {
@@ -210,12 +227,13 @@ void updatePatient() {
     if (found) {
         saveAllPatientsToFile(patients);
         cout << " Patient updated successfully.\n";
-    } else {
+    } 
+    else {
         cout << "Patient not found.\n";
     }
 }
 
-// Delete patient by name
+// Delete patient record by name
 void deletePatient() {
     vector<patient> patients = readPatientsFromFile();
     string updateName;
@@ -229,17 +247,18 @@ void deletePatient() {
     if (patients.size() < originalSize) {
         saveAllPatientsToFile(patients);
         cout << " Patient deleted successfully.\n";
-    } else {
+    } 
+    else {
         cout << " Patient not found.\n";
     }
 }
+// Search patient record by name,doctor,illness
 void searchPatients() {
     vector<patient> patients = readPatientsFromFile();
     if (patients.empty()) {
         cout << "No patient records found.\n";
         return;
     }
-
     cout << "\nSearch by:\n";
     cout << "1. Name\n2. Doctor Name\n3. Illness\nEnter choice: ";
     string choice;
@@ -263,6 +282,7 @@ void searchPatients() {
         cout << "No  patient found.\n";
     }
 }
+//sort patient record by name or age
 void sortPatients() {
     vector<patient> patients = readPatientsFromFile();
     if (patients.empty()) {
@@ -278,11 +298,13 @@ void sortPatients() {
         sort(patients.begin(), patients.end(), [](const patient &a, const patient &b) {
             return a.name < b.name;
         });
-    } else if (choice == "2") {
+    } 
+    else if (choice == "2") {
         sort(patients.begin(), patients.end(), [](const patient &a, const patient &b) {
             return a.age < b.age;
         });
-    } else {
+    } 
+    else {
         cout << "Invalid choice.\n";
         return;
     }
@@ -291,13 +313,13 @@ void sortPatients() {
         p.display();
     }
 }
+//show visit summary
 void todayVisitSummary() {
     vector<patient> patients = readPatientsFromFile();
     if (patients.empty()) {
         cout << "No patient records found.\n";
         return;
     }
-
     string todayDate;
     cout << "Enter today's date (YYYY-MM-DD): ";
     getline(cin, todayDate);
@@ -315,6 +337,7 @@ void todayVisitSummary() {
         cout << "No data found for today.\n";
     }
 }
+//main menu for clinic patient management
 int main() {
     while (true) {
         cout << "\n===== Clinic Patient Management Menu =====\n";
